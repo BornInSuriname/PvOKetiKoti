bd9986c4522505a03db12dd3dbd9a4302330342f
Fabian <fvroom@live.com>
2025-05-01 17:56:32 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,25 @@ import GridSection from '../components/GridSection'
 import Timeline from '../components/Timeline'
 import Footer from '../components/Footer'
 
-export default function Home() {
+export default function Home({ timeline }) {
   return (
     <main className="bg-white text-black min-h-screen">
-      <Hero
-        title="Herdenking en Vrijheid"
-        subtitle="PvO Keti Koti brengt het verleden naar het heden"
-        backgroundImage="/images/hero.jpg"
-      />
+      <Hero title="Herdenking en Vrijheid" subtitle="PvO Keti Koti brengt het verleden naar het heden" backgroundImage="/images/hero.jpg" />
       <GridSection />
-      <Timeline />
+      <Timeline items={timeline} />
       <Footer />
     </main>
   )
+}
+
+import { client } from '../lib/graphql'
+import { GET_TIMELINE_ITEMS } from '../lib/queries'
+
+export async function getStaticProps() {
+  try {
+    const data = await client.request(GET_TIMELINE_ITEMS)
+    return { props: { timeline: data.timelineItems.nodes || [] } }
+  } catch (err) {
+    return { props: { timeline: [] } }
+  }
 }
